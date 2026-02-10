7a13a2ea6a043e623b07e5e4a71cca3f8c30fcf9
AleksandraTatievskaia <alexschool04@mail.ru>
2026-02-10 12:12:42 +0500

    Initial Commit

---
+++ DiabetTracker/DiabetTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiabetTrackerApp.swift
+//  DiabetTracker
+//
+//  Created by Александра Татиевская on 10.02.2026.
+//
+
+import SwiftUI
+
+@main
+struct DiabetTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DiabetTrackerTests/DiabetTrackerTests.swift
@@ -0,0 +1,17 @@
+//
+//  DiabetTrackerTests.swift
+//  DiabetTrackerTests
+//
+//  Created by Александра Татиевская on 10.02.2026.
+//
+
+import Testing
+@testable import DiabetTracker
+
+struct DiabetTrackerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
